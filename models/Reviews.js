112f3687dab3df5ae01ac5425215f2e698f9e350
Dylan <dylancool16@gmail.com>
2021-03-08 01:07:09 -0500

    update review schema

--- models/Reviews.js
+++ models/Reviews.js
@@ -3,11 +3,17 @@ const mongoose = require("mongoose")
 const schema = mongoose.Schema({
 	id: Number,
 	food: Number,
+	foodCount: Number,
     drink: Number,
+	drinkCount: Number,
 	service: Number,
+	serviceCount: Number,
 	price: Number,
+	priceCount: Number,
 	noise: Number,
+	noiseCount: Number,
 	atmosphere: Number,
+	atmosphereCount: Number,
 }, {collection: "Reviews"})
 
 module.exports = mongoose.model("Review", schema)
